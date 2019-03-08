7cacfb582c216f19688b69de4e41f22a4eb48a58
lijiahao <598188200@qq.com>
2019-03-09 02:43:44 +0800

    spring设计模式-工厂模式 DH 2019年3月9日 02:43:40

--- src/main/java/com/ljh/study/factory/abstractfactory/BarcelonaTeamFactory.java
+++ src/main/java/com/ljh/study/factory/abstractfactory/BarcelonaTeamFactory.java
@@ -4,7 +4,7 @@ import com.ljh.study.factory.BarcelonaTeam;
 import com.ljh.study.factory.FootballTeam;
 
 /**
- * @description: 巴塞罗那足球队抽象工厂类创建类
+ * @description: 巴塞罗那足球队抽象工厂类创建类= =
  * @author: Jh Lee
  * @create: 2019-03-09 01:47
  **/
